f768d24e66b59284c8a6b140976dcfb6e142e8b3
Jay Lee <jay.lee@findit.nu>
2017-11-27 19:01:58 -0800

    gcm reg

--- notihub/notihub.go
+++ notihub/notihub.go
@@ -40,8 +40,17 @@ const (
     <content type="application/xml">
         <AppleRegistrationDescription xmlns:i="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://schemas.microsoft.com/netservices/2010/10/servicebus/connect">
             <Tags>{{Tags}}</Tags>
-            <DeviceToken>{{DeviceId}}</DeviceToken> 
+            <DeviceToken>{{DeviceId}}</DeviceToken>
         </AppleRegistrationDescription>
+    </content>
+</entry>`
+	AndroidRegTemplate string = `<?xml version="1.0" encoding="utf-8"?>
+<entry xmlns="http://www.w3.org/2005/Atom">
+    <content type="application/xml">
+        <GcmRegistrationDescription xmlns:i="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://schemas.microsoft.com/netservices/2010/10/servicebus/connect">
+            <Tags>{{Tags}}</Tags>
+            <GcmRegistrationId>{{DeviceId}}</GcmRegistrationId>
+        </GcmRegistrationDescription>
     </content>
 </entry>`
 )
@@ -323,10 +332,12 @@ func (h *NotificationHub) Register(r Registration) (RegistrationRes, []byte, err
 	switch r.Service {
 	case AppleFormat:
 		payload = strings.Replace(AppleRegTemplate, "{{DeviceId}}", r.DeviceId, 1)
-		payload = strings.Replace(payload, "{{Tags}}", r.Tags, 1)
+	case AndroidFormat:
+		payload = strings.Replace(AndroidRegTemplate, "{{DeviceId}}", r.DeviceId, 1)
 	default:
 		return regRes, nil, errors.New("not implemented.")
 	}
+	payload = strings.Replace(payload, "{{Tags}}", r.Tags, 1)
 
 	if r.RegistrationId != "" {
 		regPath += "/" + r.RegistrationId
